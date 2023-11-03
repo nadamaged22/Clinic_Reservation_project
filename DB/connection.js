3feb3ea1a0cd9736ab84796d51fb92df568da194
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-03 16:34:20 +0200

    change port

--- DB/connection.js
+++ DB/connection.js
@@ -5,11 +5,7 @@ const pool =new Pool({
     database:'clinic2',
     password:'nnn123nnn',
     port:3000,
-    // user:process.env.DB_USER,
-    // host:process.env.DB_HOST,
-    // database:process.env.DATABASE,
-    // password:process.env.PASSWORD,
-    // port:process.env.PORT,
+  
 })
 pool.on("connect", () => {
     console.log("Connected to DB Successfully!");
