ec64cb50354a2d49f869809b111e66ca854f8949
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-03 17:34:40 +0200

    port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const filename = __filename;
 var __dirname = path.dirname(filename)
 dotenv.config({ path: path.join(__dirname, '.env') })
 const app = express()
-const port = 5000
+const port = 8080
 initApp(app ,express)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
