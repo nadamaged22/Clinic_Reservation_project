bab59e205ce015ccd2203b6c7b95673af7f8ccfa
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-03 20:09:52 +0200

    connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,9 +1,9 @@
 const{ Pool } =require ("pg")
 const pool =new Pool({
-    user:'postgres',
-    host:"postgres://:@ep-nameless-cake-42829107.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-    database:'clinic2_1dow',
-    password:'71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb',
+    // user:'postgres',
+    host:"postgres://clinic2_1dow_user:71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb@dpg-cl2hl148s0fs738agid0-a/clinic2_1dow",
+    // database:'clinic2_1dow',
+    // password:'71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb',
     port:5432,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0
