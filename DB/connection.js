b539fcfd1ccc9afbef0ac6cda7c0bee2650fd2a0
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-03 22:19:24 +0200

    edit in db

--- DB/connection.js
+++ DB/connection.js
@@ -1,10 +1,15 @@
 const{ Pool } =require ("pg")
 const pool =new Pool({
+    host:process.env.POSTGRESQL_ADDON_HOST,
+    database:process.env.POSTGRESQL_ADDON_DB,
+    user:process.env.POSTGRESQL_ADDON_USER,
+    port:process.env.POSTGRESQL_ADDON_PORT,
+    password:process.env.POSTGRESQL_ADDON_PASSWORD,
     // user:'postgres',
-    host:"postgres://clinic2_1dow_user:71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb@dpg-cl2hl148s0fs738agid0-a/clinic2_1dow",
+    // host:"postgres://clinic2_1dow_user:71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb@dpg-cl2hl148s0fs738agid0-a/clinic2_1dow",
     // database:'clinic2_1dow',
     // password:'71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb',
-    port:5432,
+    // port:5432,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0
   
