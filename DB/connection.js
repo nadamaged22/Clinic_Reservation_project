b054c8f5a985b165224f1de4d23be5e1e22be22e
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-03 19:39:42 +0200

    change host

--- DB/connection.js
+++ DB/connection.js
@@ -1,10 +1,10 @@
 const{ Pool } =require ("pg")
 const pool =new Pool({
     user:'postgres',
-    host:'localhost',
-    database:'clinic2',
-    password:'nnn123nnn',
-    port:3000,
+    host:'psql "postgres://:@ep-nameless-cake-42829107.us-east-1.postgres.vercel-storage.com:5432/verceldb"',
+    database:'clinic2_1dow',
+    password:'71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb',
+    port:5432,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0
   
