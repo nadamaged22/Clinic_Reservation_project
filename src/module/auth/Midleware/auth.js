74081dbdab4c84372e7c29d8f4e57cc7db43a58c
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-05 00:18:24 +0200

    add auth and add query for credinatinals

---
+++ src/module/auth/Midleware/auth.js
@@ -0,0 +1,51 @@
+const jwt =require("jsonwebtoken");
+const pool = require("../../../../DB/connection");
+const queries =require('../auth.queries');
+const util = require('util');
+const queryPromise = util.promisify(pool.query).bind(pool);
+
+
+const roles={
+    Patient:'Patient',
+    Doctor:'Doctor'
+}
+Object.freeze(roles)
+
+const auth = (roles=[])=>{
+   return  async (req, res, next) => {
+        try {
+            const { authorization } = req.headers;
+            if (!authorization?.startsWith(process.env.BEARER_KEY)) {
+                return res.json({ message: "In-valid bearer key" })
+            }
+            const token = authorization.split(process.env.BEARER_KEY)[1]
+            if (!token) {
+                return res.json({ message: "In-valid token" })
+            }
+            const decoded = jwt.verify(token, process.env.TOKEN_SIGNATURE)
+            if (!decoded?.id) {
+                return res.json({ message: "In-valid token payload" })
+            }
+            const result=await queryPromise(queries.CheckUserCredintials,[decoded.id])
+
+            const authUser = result.rows[0]
+            if (!authUser) {
+                return res.json({ message: "Not register account" })
+            }
+            if(!roles.includes(authUser.role)){
+                return res.status(401).json({message:'YOU ARE NOT AUTHORIZED!'})
+            }
+            req.user = authUser;
+            return next()
+        } catch (error) {
+            return res.json({ message: "Catch error" , err:error?.message })
+        }
+
+    }
+}
+module.exports={
+    roles,
+    auth,
+    
+}
+
--- src/module/auth/auth.queries.js
+++ src/module/auth/auth.queries.js
@@ -1,11 +1,13 @@
 const CheckEmailExist="SELECT s FROM users s WHERE s.email=$1";
 const adduser="INSERT INTO users (name,email,password,role) VALUES($1,$2,$3,$4)";
 const loginCheck="SELECT email, password ,id FROM users WHERE email= $1";
+const CheckUserCredintials="SELECT id, name, email,role FROM users WHERE id =$1";
 
 
 
 module.exports={
     CheckEmailExist,
     adduser,
-    loginCheck
+    loginCheck,
+    CheckUserCredintials
 };
