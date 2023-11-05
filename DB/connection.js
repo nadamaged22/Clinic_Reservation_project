6d07b7cecd058b75766567033ce491017ae0e451
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-05 17:43:11 +0200

    appointmrnt module

--- DB/connection.js
+++ DB/connection.js
@@ -5,11 +5,6 @@ const pool =new Pool({
     user:process.env.POSTGRESQL_ADDON_USER,
     port:process.env.POSTGRESQL_ADDON_PORT,
     password:process.env.POSTGRESQL_ADDON_PASSWORD,
-    // user:'postgres',
-    // host:"postgres://clinic2_1dow_user:71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb@dpg-cl2hl148s0fs738agid0-a/clinic2_1dow",
-    // database:'clinic2_1dow',
-    // password:'71jaS9kFJeyOUyZxXBaWBqY5EaNiaVrb',
-    // port:5432,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0
   
