ad437800929f8bc57c3b4f066ced11f860105946
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-05 03:17:21 +0200

    doctor id change

--- src/module/slots/controller/slots.js
+++ src/module/slots/controller/slots.js
@@ -14,10 +14,11 @@ const addslot=asyncHandler(async(req,res,next)=>{
     if(usernotfound){
         return next (new Error("THIS USER IS NOT EXIST!",{cause:404}))
     }
-    console.log(req.user.id)
+    console.log(req.user.is)
     const{date,hour}=req.body
     const Doctorid=await queryPromise(queries.DoctorID,[req.user.id])
-    const result=await queryPromise(queries.addslot,[Doctorid,date,hour]);
+    const doctorIdValue = Doctorid.rows[0].doctor_id;
+    const result=await queryPromise(queries.addslot,[doctorIdValue,date,hour]);
     const slot=result.rows[0]
     res.status(201).json({message:"SLOT ADDRD SUCCES!",slot})
 })
