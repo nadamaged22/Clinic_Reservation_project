bd9b32e775bb6307f07b36bc6249ddaaaa15fb08
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-05 21:01:18 +0200

    change in dr

--- src/module/doctors/controller/doctors.js
+++ src/module/doctors/controller/doctors.js
@@ -8,7 +8,7 @@ const queryPromise = util.promisify(pool.query).bind(pool);
 const getAllDoctors=asyncHandler(async(req,res,next)=>{
     const result=await queryPromise(queries.getAllDoctors)
     //do this to retrive all the doctors name with dr before the name
-   const modifiedDoctors = result.map(doctor => {
+   const modifiedDoctors = result.rows.map(doctor => {
     return {
       ...doctor,
       name: `Dr. ${doctor.name}`
