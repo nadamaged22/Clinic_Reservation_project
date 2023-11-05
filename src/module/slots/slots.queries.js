51ae3f85dab3cf1797e85c4875219ee6296bb06f
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-05 22:55:15 +0200

    change in schedule query

--- src/module/slots/slots.queries.js
+++ src/module/slots/slots.queries.js
@@ -1,7 +1,7 @@
 const CheckUserId="SELECT s FROM users s WHERE s.id=$1";
 const DoctorID="SELECT doctor_id FROM doctors WHERE user_id =$1";
 const addslot="INSERT INTO schedules (doctor_id,date,hour) VALUES($1,$2,$3)";
-const getSlotsByDRId="SELECT s.date,s.hour FROM schedules s WHERE s.doctor_id = $1";
+const getSlotsByDRId="SELECT s.schedule_id,s.date,s.hour FROM schedules s WHERE s.doctor_id = $1";
 const checkDrId="SELECT d FROM doctors d WHERE doctor_id =$1";
  
  
