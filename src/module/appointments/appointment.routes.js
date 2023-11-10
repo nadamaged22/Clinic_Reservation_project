97f0f22dfcdd0f85cf07c669c201389790ece55f
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-10 23:37:35 +0200

    show appointment

--- src/module/appointments/appointment.routes.js
+++ src/module/appointments/appointment.routes.js
@@ -5,5 +5,6 @@ const { auth } = require("../../Midleware/auth");
 const router = Router()
 
 router.post('/add/:SlotID',auth(endpoints.AppointmentCrud),pc.CreateAppointment)
+router.get('/show',auth(endpoints.AppointmentCrud),pc.ShowAppointment)
 
 module.exports=router
--- src/module/appointments/controller/appointment.js
+++ src/module/appointments/controller/appointment.js
@@ -7,7 +7,6 @@ const CreateAppointment=asyncHandler(async(req,res,next)=>{
     try{
         client=await pool.connect()
         const SlotID=parseInt(req.params.SlotID)
-
         const CreateAppointment=await client.query(queries.CreateAppointment,[SlotID,req.user.id])
         const appointment = CreateAppointment.rows[0]
         res.status(201).json({message:"YOUR APPOINTMENT RESERVED SUCCESSFULLY!",appointment})
@@ -22,7 +21,25 @@ const CreateAppointment=asyncHandler(async(req,res,next)=>{
     }
 
 })
+const ShowAppointment=asyncHandler(async(req,res,next)=>{
+    let client
+    try{
+        client=await pool.connect()
+        const ShowAppointment=await client.query(queries.ShowAppointment,[req.user.id])
+        const Appointments=ShowAppointment
+        res.status(200).json({message:"DONE",Appointments})
+       
+    }catch (error) {
+        next(error);
+    } finally {
+        if (client) {
+            client.release(); // Release the client back to the pool
+            // await client.end()
+        }
+    }
+})
 
 module.exports={
     CreateAppointment,
+    ShowAppointment
 }
