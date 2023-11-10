ffa7d913560b52c31c8be855c6ce2e37cbd0ad64
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-11 01:49:25 +0200

    ch

--- src/module/appointments/appointment.queries.js
+++ src/module/appointments/appointment.queries.js
@@ -1,5 +1,5 @@
 const CreateAppointment="WITH inserted_appointment AS (INSERT INTO appointments (appointment_date, appointment_time, doctor_id, schedule_id, patient_id) SELECT s.date, s.hour, s.doctor_id, $1 AS schedule_id, u.id AS patient_id FROM schedules s JOIN users u ON u.role = 'Patient' WHERE s.schedule_id = $1 AND u.id = $2 RETURNING schedule_id) UPDATE schedules SET reserved = true WHERE schedule_id IN (SELECT schedule_id FROM inserted_appointment);"
-const ShowAppointment="SELECT a.appointment_id, a.appointment_date,a.appointment_time,u.name AS doctor_name FROM appointments a JOIN users u ON a.doctor_id = u.id WHERE a.patient_id =$1;"
+const ShowAppointment="SELECT a.appointment_id,a.patient_id,d.doctor_id,u.name AS doctor_name FROM appointments a JOIN doctors d ON a.doctor_id = d.doctor_id JOIN users u ON d.user_id = u.id WHERE a.patient_id =$1";
 
 module.exports={
     CreateAppointment,
