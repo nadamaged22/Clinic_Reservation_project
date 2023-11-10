d112760a46d8d8e98f8ab72b68ab2412e653adf4
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-10 02:37:54 +0200

    handle errors

--- src/module/slots/controller/slots.js
+++ src/module/slots/controller/slots.js
@@ -6,40 +6,51 @@ const queries =require('../slots.queries');
 const queryPromise = util.promisify(pool.query).bind(pool);
 
 const addslot=asyncHandler(async(req,res,next)=>{
-    const CheckUserExist=await queryPromise(queries.CheckUserId,[req.user.id])
-    const usernotfound=!CheckUserExist.rows.length
-    if(usernotfound){
-        return next (new Error("THIS USER IS NOT EXIST!",{cause:404}))
-    }
-    const{date,hour}=req.body
-    const Doctorid=await queryPromise(queries.DoctorID,[req.user.id])
-    const doctorIdValue = Doctorid.rows[0].doctor_id;
-    const checkSelectedSlots=await queryPromise(queries.getSlotsByDRId,[req.user.id])
-    checkSelectedSlots.rows.forEach(row => {
-        if(row.date === date && row.hour === hour && row.doctor_id=== req.user.id){
-            return next(new Error("YOU ALREADY CHOSE THIS SLOT!", { cause: 409 }));
+    let client;
+    try{
+        client = await pool.connect();
+        const CheckUserExist=await client.query(queries.CheckUserId,[req.user.id])
+        const usernotfound=!CheckUserExist.rows.length
+        if(usernotfound){
+            return next (new Error("THIS USER IS NOT EXIST!",{cause:404}))
         }
-    });
-    const result=await queryPromise(queries.addslot,[doctorIdValue,date,hour]);
-    const slot = result.rows[0]
-    res.status(201).json({message:"SLOT ADDED SUCCES!",slot})
-})
+        const{date,hour}=req.body
+        const Doctorid=await client.query(queries.DoctorID,[req.user.id])
+        const doctorIdValue = Doctorid.rows[0].doctor_id;
+        const checkSelectedSlots=await client.query(queries.getSlotsByDRId,[req.user.id])
+        checkSelectedSlots.rows.forEach(row => {
+            if(row.date === date && row.hour === hour && row.doctor_id=== req.user.id){
+                return next(new Error("YOU ALREADY CHOSE THIS SLOT!", { cause: 409 }));
+            }
+        });
+        const result=await client.query(queries.addslot,[doctorIdValue,date,hour]);
+        const slot = result.rows[0]
+        res.status(201).json({message:"SLOT ADDED SUCCES!",slot})
+       
+    }catch (error) {
+        next(error);
+    } finally {
+        if (client) {
+            client.release(); // Release the client back to the pool
+            // await client.end()
+        }
+    }
+    
 
+
+
+});
 const getSlotsByDRId=asyncHandler(async(req,res,next)=>{
     const id=parseInt(req.params.id)
-    const CheckUserExist=await queryPromise(queries.checkDrId,[id])
+    const CheckUserExist=await client.query(queries.checkDrId,[id])
     const usernotfound=!CheckUserExist.rows.length
     if(usernotfound){
         return next (new Error("THIS DOCTOR IS NOT EXIST!",{cause:404}))
     }
-    const result=await queryPromise(queries.getSlotsByDRId,[id])
+    const result=await client.query(queries.getSlotsByDRId,[id])
     const AvaliableSlots=result.rows
     res.status(200).json({message:"DONE",AvaliableSlots})
-
-
-
 })
-
 
 
 
