826bfc141014f6317d442b75c197a13a71430cf2
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-10 03:43:56 +0200

    changes

--- src/module/slots/slots.queries.js
+++ src/module/slots/slots.queries.js
@@ -3,7 +3,7 @@ const DoctorID="SELECT doctor_id FROM doctors WHERE user_id =$1";
 const addslot="INSERT INTO schedules (doctor_id,date,hour) VALUES($1,$2,$3)";
 const getSlotsByDRId="SELECT s.schedule_id,s.date,s.hour FROM schedules s WHERE s.doctor_id = $1";
 const checkDrId="SELECT d FROM doctors d WHERE doctor_id =$1";
-const checkSelectedSlots="SELECT s.date,s.hour FROM schedules s WHERE s.doctor_id = $1";
+const checkSelectedSlots="SELECT s.date,s.hour FROM schedules s WHERE s.doctor_id = $1 AND s.date=$2 AND s.hour=$3";
  const UserID="SELECT user_id  FROM doctors WHERE doctor_id =$1";
 
 
