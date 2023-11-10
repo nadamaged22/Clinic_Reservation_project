2519ac89ddab05cdd3971d86ac377ee68cc938f1
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-10 22:40:18 +0200

    appointment routes

--- src/module/appointments/appointment.routes.js
+++ src/module/appointments/appointment.routes.js
@@ -4,6 +4,6 @@ const { endpoints } = require("./appointment.endpoints");
 const { auth } = require("../../Midleware/auth");
 const router = Router()
 
-router.post('/add',auth(endpoints.AppointmentCrud),pc.CreateAppointment)
+router.post('/add/:SlotID',auth(endpoints.AppointmentCrud),pc.CreateAppointment)
 
 module.exports=router
