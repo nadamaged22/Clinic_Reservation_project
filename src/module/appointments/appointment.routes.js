007140c67adb3f7afb6f42af1803dec4ae691db5
nadamaged <119138499+nadamaged22@users.noreply.github.com>
2023-11-12 17:48:52 +0200

    routes edit

--- src/module/appointments/appointment.routes.js
+++ src/module/appointments/appointment.routes.js
@@ -6,6 +6,6 @@ const router = Router()
 
 router.post('/add/:SlotID',auth(endpoints.AppointmentCrud),pc.CreateAppointment)
 router.get('/show',auth(endpoints.AppointmentCrud),pc.ShowAppointment)
-router.put('/update',auth(endpoints.AppointmentCrud),pc.EditAppointment)
+router.put('/update/:DoctorID/:SlotID/:appointmentID',auth(endpoints.AppointmentCrud),pc.EditAppointment)
 
 module.exports=router
